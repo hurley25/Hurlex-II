4a328d650becfe3c115032bd8bc0bad07f8d5631
hurley25 <liuhuan1992@gmail.com>
2014-11-13 14:33:54 +0800

    调度函数微调

--- src/arch/i386/sched/sched.c
+++ src/arch/i386/sched/sched.c
@@ -37,21 +37,26 @@ void schedule(void)
         local_intr_store(intr_flag);
         {
                 current->need_resched = false;
-                last = (current == glb_idle_task) ? &task_list : &(current->list);
-                le = last;
-                do {
-                        if ((le = le->next) != &task_list) {
+
+                // 如果当前是内核调度线程在执行，就从任务链起始开始，否则从当前任务开始
+                le = last = (current == glb_idle_task) ? &task_list : &(current->list);
+                for (le = le->next; le != last; le = le->next) {
+                        if (le != &task_list) {
                                 task_next = le_to_task(le);
                                 if (task_next->state == TASK_RUNNABLE) {
                                         break;
                                 }
                         }
-                } while (le != last);
+                } 
 
+                // 未找到可运行的任务
                 if (!task_next || task_next->state != TASK_RUNNABLE) {
                         task_next = glb_idle_task;
                 }
+
+                // 时间片递增
                 task_next->runs_time++;
+
                 if (task_next != current) {
                         task_run(task_next);
                 }
@@ -61,7 +66,7 @@ void schedule(void)
 
 void wakeup_task(struct task_struct *task)
 {
-        if (task->state != TASK_ZOMBIE && task->state != TASK_RUNNABLE) {
+        if (task->state != TASK_ZOMBIE) {
                 task->state = TASK_RUNNABLE;
         }
 }
