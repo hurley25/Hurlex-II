3a235d79d0e393bf1203831a9bbab1ebf8eda45f
hurley25 <liuhuan1992@gmail.com>
2015-02-06 10:59:42 +0800

    调整代码结构
    Signed-off-by: hurley25 <liuhuan1992@gmail.com>

---
+++ src/fs/sfs/sfs_inode.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sfs_inode.c
+ *
+ *    Description:  sfs inode 相关
+ *
+ *        Version:  1.0
+ *        Created:  2015年02月06日 10时57分58秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Qianyi.lh (liuhuan), qianyi.lh@alibaba-inc.com
+ *        Company:  Alibaba-Inc Aliyun
+ *
+ * =====================================================================================
+ */
+
+#include "sfs_inode.h"
+
---
+++ src/fs/sfs/sfs_inode.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sfs_inode.h
+ *
+ *    Description:  sfs inode 相关
+ *
+ *        Version:  1.0
+ *        Created:  2015年02月06日 10时57分11秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Qianyi.lh (liuhuan), qianyi.lh@alibaba-inc.com
+ *        Company:  Alibaba-Inc Aliyun
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_SFS_INODE_H
+#define INCLUDE_SFS_INODE_H
+
+
+
+#endif  // INCLUDE_SFS_INODE_H
+
