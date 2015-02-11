c3c224cbbccfa04861185eb23699fca5c1e96476
hurley25 <liuhuan1992@gmail.com>
2015-02-11 13:56:29 +0800

    修改sfs
    Signed-off-by: hurley25 <liuhuan1992@gmail.com>

--- src/fs/sfs/sfs.c
+++ src/fs/sfs/sfs.c
@@ -34,27 +34,6 @@ struct filesystem fs_sfs = {
 // 读取super_block
 static struct super_block *sfs_read_super(struct super_block *sb)
 {
-        // 初始化根结点 inode
-        struct inode *inode = alloc_inode();
-        bzero(inode, sizeof(struct inode));
-        inode->i_sb = sb;
-
-        // 初始化根结点 dentry
-        struct dentry *dentry = alloc_dentry();
-        atomic_set(&(dentry->d_count), 0);
-        INIT_LIST_HEAD(&(dentry->d_brother));
-        INIT_LIST_HEAD(&(dentry->d_subdirs));
-        dentry->d_status = 1;
-        dentry->d_parent = NULL;
-        dentry->d_sb = sb;
-        dentry->d_inode = inode;
-        dentry->is_mounted = 0;
-        strcpy(dentry->d_name, "/");
         
-        // 设置超级块对应的文件系统类型和根目录
-        sb->s_type = SFS_T;
-        sb->s_root = dentry;
-
-        return sb;
 }
 
